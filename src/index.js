4bb10ed321c3956e43508ede7c653f42f033614b
bingym <22383342+bingym@users.noreply.github.com>
2024-06-19 17:44:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.bingym.com": dockerHub,
+  "quay.bingym.com": "https://quay.io",
+  "gcr.bingym.com": "https://gcr.io",
+  "k8s-gcr.bingym.com": "https://k8s.gcr.io",
+  "k8s.bingym.com": "https://registry.k8s.io",
+  "ghcr.bingym.com": "https://ghcr.io",
+  "cloudsmith.bingym.com": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.bingym.com": dockerHub,
 };
 
 function routeByHosts(host) {
